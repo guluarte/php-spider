b2a4e7a0180af909da97d5643a82b45ad4cfc6ac
Rodolfo Guluarte <rodolfo@qrubit.com>
2013-09-26 03:15:19 -0700

    Added curl timeout

--- pinterest.php
+++ pinterest.php
@@ -145,7 +145,10 @@ function getHeadHtml($url) {
 	curl_setopt($ch, CURLOPT_URL, $url); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	 
 	$head = curl_exec($ch); 
 	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
 	curl_close($ch);
